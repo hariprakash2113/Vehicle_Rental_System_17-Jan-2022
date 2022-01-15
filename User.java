731ae543c3e9551d572d8071dccbb21886c9bff1
Hari Prakash V <hariprakash2113@gmail.com>
2022-01-15 22:16:39 +0530

    Users can now login and also added option for forgot password

--- User.java
+++ User.java
@@ -28,7 +28,7 @@ public class User {
         }
 
         else if (n == 2) {
-            // userLogin();
+            userLogin();
         }
 
         else if (n == 3) {
@@ -42,6 +42,75 @@ public class User {
             login();
         }
 
+    }
+
+    static int findUserInd(String email) {
+        for (int i = 0; i < Main.users.size(); i++) {
+            if (Main.users.get(i).email.equals(email)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    static void userLogin() {
+        System.out.print("\033[H\033[2J");
+        System.out.println("-----Custumer Login-----");
+        System.out.print("Enter Email or 0 to exit : ");
+        String email = Main.sc.nextLine();
+        if(email.equals("0")){
+            login();
+        }
+        int user_index = findUserInd(email);
+        if (user_index == -1) {
+            System.out.println("User with Email ID =>  " + email + " not Found !");
+            System.out.println("Press any key to continue......");
+            Main.sc.nextLine();
+            userLogin();
+        }
+        System.out.print("Enter Password or 0 to Forgot Password : ");
+        String password = Main.sc.nextLine();
+        if (password.equals("0")) {
+            forgetPassword(user_index);
+        }
+        if (Main.users.get(user_index).pass.equals(password)) {
+            //userPage(user_index);
+            System.out.println("userPage(user_index);");
+        } else {
+            System.out.println("Incorrect password !\nTry Again");
+            System.out.println("Press any key to continue......");
+            Main.sc.nextLine();
+            userLogin();
+        }
+    }
+
+    static void forgetPassword(int user_index) {
+        System.out.print("\033[H\033[2J");
+        System.out.print("Enter Your Mobile number or 0 to Exit : ");
+        String mobile = Main.sc.nextLine();
+        if (mobile.equals("0")) {
+            userLogin();
+        }
+        if (Main.users.get(user_index).mobile.equals(mobile)) {
+            System.out.print("Enter OTP : ");
+            String otp = Main.sc.nextLine();
+            if (otp.equals("123456")) {
+                System.out.println("Your Password is => " + Main.users.get(user_index).pass);
+                System.out.println("Press any key to continue......");
+                Main.sc.nextLine();
+                login();
+            } else {
+                System.out.println("Wrong OTP !\nTry again");
+                System.out.println("Press any key to continue......");
+                Main.sc.nextLine();
+                forgetPassword(user_index);
+            }
+        } else {
+            System.out.println("Entered Mobile number Doesn't match");
+            System.out.println("Press any key to continue......");
+            Main.sc.nextLine();
+            forgetPassword(user_index);
+        }
     }
 
     static void userSignUp() {
